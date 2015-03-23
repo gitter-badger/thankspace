fa2e8671789e46ee1b574662fa092ef13a05bcdc
antoniputra <akiddcode@gmail.com>
2015-03-23 14:12:54 +0700

    .gitignore is now working

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('WHITERUN'),
+	'local' => array('WHITERUN', 'antoni-K46CB'),
 	
 	'production' => array(''),
 

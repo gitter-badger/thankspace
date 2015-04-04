c12ce78ba7ea54b883f2ee30eaa52819a929791b
Whiterun <winterhold89@gmail.com>
2015-04-04 20:24:41 +0700

    #

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('WHITERUN', 'antoni-K46CB', 'Human-PC'),
+	'local' => array('WHITERUN', 'antoni-K46CB', 'Human'),
 	
 	'production' => array(''),
 

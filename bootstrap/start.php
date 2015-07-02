1e5920ec6386b85e7cdeef7c1833a28741f14184
Deny <denydeyn@gmail.com>
2015-07-02 16:21:00 +0700

    email admin function

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('WHITERUN', 'antoni-K46CB', 'Human', 'Ghost'),
+	'local' => array('WHITERUN', 'antoni-K46CB', 'Human', 'Ghost-PC'),
 	
 	'production' => array('server1'),
 

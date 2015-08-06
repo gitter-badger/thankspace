99c9a702f7b9d306df8bd416448da9ad412d5c46
Rizal-Afani <ahmadrizalafani@gmail.com>
2015-08-06 08:59:34 +0700

    add column n table for refferal feature

---
+++ app/database/migrations/2015_08_06_085127_create_space_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpaceTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('space', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->string('type')->default('credit');
+			$table->integer('nominal');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('space');
+	}
+
+}
